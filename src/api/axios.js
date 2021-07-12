67f0290e793a60b0217cf49fb19b34fd7f29285f
daeun-react <smile_daeun@naver.com>
2021-07-13 00:50:15 +0900

    axios file console changed

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const getList = async () => {
+  console.log(process.env.NODE_ENV, process.env.REACT_APP_API_URL);
   if (process.env.NODE_ENV === "development") {
     const response = await axios.get(`${process.env.REACT_APP_API_URL}/mails`);
     return response.data;
