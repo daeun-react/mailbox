c96468c61ddfb22ac0bc107758b52bc61a757882
daeun-react <smile_daeun@naver.com>
2021-07-13 00:54:21 +0900

    axios prod url change

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
 export const getList = async () => {
-  console.log(process.env.NODE_ENV, process.env.REACT_APP_API_URL);
-  if (process.env.NODE_ENV === "development") {
-    const response = await axios.get(`${process.env.REACT_APP_API_URL}/mails`);
-    return response.data;
-  } else {
-    const response = await axios.get(`${process.env.REACT_APP_API_URL}`);
-    // console.log(response.data.mails);
-    return response.data.mails;
-  }
+  //if (process.env.NODE_ENV === "development") {
+  //  const response = await axios.get(`${process.env.REACT_APP_API_URL}/mails`);
+  //  return response.data;
+  //} else {
+  const response = await axios.get(
+    `https://raw.githubusercontent.com/daeun-react/mailbox/main/server/data.json`
+  );
+  return response.data.mails;
+  //}
 };
 
 export const getListById = async (id) => {
